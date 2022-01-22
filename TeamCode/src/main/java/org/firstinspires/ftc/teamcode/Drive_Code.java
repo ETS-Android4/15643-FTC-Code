743b245c72d708e40ec2ba490705f1279dbcfd90
Barry Computer <apc51104@gmail.com>
2022-01-22 08:38:16 -0500

    commit from school

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive_Code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive_Code.java
@@ -57,7 +57,7 @@ import com.qualcomm.robotcore.util.Range;
 public class Drive_Code extends OpMode {
 
 
-// this is a
+// this is a sentence
 
 
     private DcMotor frontRight, frontLeft, backLeft, backRight;
